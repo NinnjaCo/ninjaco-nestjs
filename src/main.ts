e4e526f4c546f2e2781f849f792fd12fd441ac39
Murf-y <charbelfayad64@gmail.com>
2023-04-03 11:23:44 +0300

    style: add comments to the code

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,23 @@ import { AppModule } from './modules/app/app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import * as bodyParser from 'body-parser'
 
+/**
+ * Bootstrap the NestJS application
+ *
+ * @returns {Promise<void>}
+ * @memberof Main
+ * @method bootstrap
+ */
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
 
+  // Enable shutdown hooks
   app.enableShutdownHooks()
 
   // Body Parser limit
   app.use(bodyParser.json({ limit: '5mb' }))
   app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }))
+
   await app.listen(3200)
 }
 bootstrap()
