8093ef7d71411b848505bab0a5281a7881151a21
Murf-y <charbelfayad64@gmail.com>
2023-04-03 11:24:53 +0300

    feat: add validation pipes for Body request validation using pipes

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './modules/app/app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import * as bodyParser from 'body-parser'
+import { HttpException, HttpStatus, ValidationError, ValidationPipe } from '@nestjs/common'
 
 /**
  * Bootstrap the NestJS application
@@ -23,6 +24,30 @@ async function bootstrap() {
   // Globale Prefix /api/...
   app.setGlobalPrefix('api')
 
+  // Add validation pipes for class-validator and class-transformer packages
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      exceptionFactory: (errors: ValidationError[]) =>
+        new HttpException(
+          {
+            status: HttpStatus.BAD_REQUEST,
+            errors: errors.reduce(
+              (accumulator, currentValue) => ({
+                ...accumulator,
+                [currentValue.property]: Object.values(currentValue.constraints).join(', '),
+              }),
+              {}
+            ),
+          },
+          HttpStatus.BAD_REQUEST
+        ),
+    })
+  )
   await app.listen(3200)
 }
 bootstrap()
