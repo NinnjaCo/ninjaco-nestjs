a2284a161bfdb4d963cbe111107b9d7fe49aa8fc
Murf-y <charbelfayad64@gmail.com>
2023-04-03 11:21:07 +0300

    feat: enable shutdown hooks

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './modules/app/app.module'
+import { NestExpressApplication } from '@nestjs/platform-express'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+
+  app.enableShutdownHooks()
   await app.listen(3200)
 }
 bootstrap()
