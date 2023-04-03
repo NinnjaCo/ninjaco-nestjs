9cae8c091b95b2b2bf5c8644fb9cc137c78a4212
Murf-y <charbelfayad64@gmail.com>
2023-04-03 11:22:33 +0300

    feat: add body parser

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './modules/app/app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
+import * as bodyParser from 'body-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
 
   app.enableShutdownHooks()
+
+  // Body Parser limit
+  app.use(bodyParser.json({ limit: '5mb' }))
+  app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }))
   await app.listen(3200)
 }
 bootstrap()
