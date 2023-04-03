f957a4bd6ec29bf37e9a8db153089f171bf32724
Murf-y <charbelfayad64@gmail.com>
2023-04-03 14:49:53 +0300

    feat: add config validation schema using JOI

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,19 @@
 import { AppController } from './app.controller'
+import { ConfigModule } from '@nestjs/config'
 import { LoggerMiddleware } from '../../common/middleware/logger.middleware'
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common'
+import { validateConfig } from 'modules/config'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      validate: (config: Record<string, any>) => {
+        return validateConfig(config)
+      },
+    }),
+  ],
   controllers: [AppController],
 })
 export class AppModule implements NestModule {
---
+++ src/modules/config/config.schema.ts
@@ -0,0 +1,46 @@
+import * as Joi from 'joi'
+
+const configSchema = Joi.object({
+  MONGODB_PORT: Joi.number().required().default(27017),
+  MONGODB_URL: Joi.string().default('mongodb://localhost:27017/ninjaco'),
+  MONGODB_URL_E2E_TEST: Joi.string().default('mongodb://localhost:27017/testdb'),
+  MONGODB_NAME: Joi.string().required().default('ninjaco'),
+  MONGODB_HOST: Joi.string().required(),
+  MONGODB_USERNAME: Joi.string().required(),
+  MONGODB_PASSWORD: Joi.string().required(),
+
+  BUCKET_NAME: Joi.string().required(),
+  MINIO_ENDPOINT: Joi.string().required(),
+  MINIO_CONSOLE_PORT: Joi.number().required().default(9001),
+  MINIO_API_PORT: Joi.number().required().default(9000),
+  MINIO_ROOT_USER: Joi.string().required(),
+  MINIO_ROOT_PASSWORD: Joi.string().required(),
+
+  MAIL_HOST: Joi.string().required(),
+  MAIL_PORT: Joi.number().required(),
+  MAIL_IGNORE_TLS: Joi.boolean().default(true),
+  MAIL_SECURE: Joi.boolean().default(false),
+  MAIL_USERNAME: Joi.string().allow('').required(),
+  MAIL_PASSWORD: Joi.string().allow('').required(),
+
+  NEST_PORT: Joi.number().default(3200),
+})
+
+/**
+ * Custom function to validate environment variables. It takes an object containing environment variables as input and outputs validated environment variables.
+ *
+ * @param {Record<string, any>} config - Description of the parameter.
+ * @returns {Record<string, any>} Description of the return value.
+ * @throws {Error} Description of the exception.
+ */
+export function validateConfig(config: Record<string, any>) {
+  const { error, value } = configSchema.validate(config, {
+    allowUnknown: true,
+    cache: true,
+    convert: true,
+  })
+  if (error) {
+    throw new Error(`Config validation error: ${error.message}`)
+  }
+  return value
+}
