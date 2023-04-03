06df03a56a86985ba81ca8818f61c4f42fafcfe6
Murf-y <charbelfayad64@gmail.com>
2023-04-03 14:52:05 +0300

    feat: add mail service

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -1,9 +1,9 @@
-import { MailerModule } from '@nestjs-modules/mailer'
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter'
-import { Module } from '@nestjs/common'
-import { MailService } from './mail.service'
-import { join } from 'path'
 import { ConfigService } from '@nestjs/config'
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter'
+import { MailService } from './mail.service'
+import { MailerModule } from '@nestjs-modules/mailer'
+import { Module } from '@nestjs/common'
+import { join } from 'path'
 
 @Module({
   imports: [
