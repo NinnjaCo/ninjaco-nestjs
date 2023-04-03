c16297290f37124e2cf22ef1f9a5a5fada5933a6
Murf-y <charbelfayad64@gmail.com>
2023-04-03 15:08:25 +0300

    feat: add minio module to app module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config'
 import { LoggerMiddleware } from '../../common/middleware/logger.middleware'
 import { MailModule } from '../mail/mail.module'
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common'
+import { MinioClientModule } from '../minio/minio-client.module'
 import { validateConfig } from '../config'
 
 @Module({
@@ -15,6 +16,7 @@ import { validateConfig } from '../config'
       },
     }),
     MailModule,
+    MinioClientModule,
   ],
   controllers: [AppController],
 })
