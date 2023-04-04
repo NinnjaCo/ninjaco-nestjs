ca75f3405e8f093908e4d90c8379166169d95e4c
Murf-y <charbelfayad64@gmail.com>
2023-04-04 16:23:01 +0300

    feat: add public decorator

--- src/modules/auth/guards/auth.guard.ts
+++ src/modules/auth/guards/auth.guard.ts
@@ -1,13 +1,19 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
+import { IS_PUBLIC_KEY } from '../../../common/decorators/public.decorator'
 import { JwtService } from '@nestjs/jwt'
-import { UNAUTHORIZED_EXCEPTION_MESSAGE } from 'common/constants'
+import { UNAUTHORIZED_EXCEPTION_MESSAGE } from '../../../common/constants'
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private jwtService: JwtService, private configService: ConfigService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = context.getHandler()[IS_PUBLIC_KEY]
+    if (isPublic) {
+      return true
+    }
+
     const request = context.switchToHttp().getRequest()
     const token = this.extractTokenFromHeader(request)
     if (!token) {
@@ -21,14 +27,25 @@ export class AuthGuard implements CanActivate {
       // so that we can access it in our route handlers
       request['user'] = payload
     } catch {
-      throw new UnauthorizedException()
+      throw new UnauthorizedException(UNAUTHORIZED_EXCEPTION_MESSAGE)
     }
     return true
   }
 
+  /**
+   * Extracts the token from the request header
+   * @param request
+   * @returns the token or undefined if it doesn't exist
+   */
   private extractTokenFromHeader(request: Request): string | undefined {
-    const header: Headers = request.headers
-    const [type, token] = header.get('authorization').split(' ')
-    return type === 'Bearer' ? token : undefined
+    const authorization = request.headers['authorization']
+    if (!authorization) {
+      return undefined
+    }
+    const [bearer, token] = authorization.split(' ')
+    if (bearer !== 'Bearer' || !token) {
+      return undefined
+    }
+    return token
   }
 }
