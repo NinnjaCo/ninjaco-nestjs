47945dbad5e1158a28cea0c313f1161ef7c9731a
Murf-y <charbelfayad64@gmail.com>
2023-04-05 11:12:28 +0300

    feat: add auth access token guard globaly

--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -1,10 +1,12 @@
 import { ApiGlobalResponse } from '../../common/decorators/api-global-response.decorators'
 import { ApiTags } from '@nestjs/swagger'
 import { Controller, Get } from '@nestjs/common'
+import { Public } from '../../common/decorators/public.decorator'
 
 @Controller()
 @ApiTags('Health')
 export class AppController {
+  @Public()
   @ApiGlobalResponse(String, {
     description: 'OK',
   })
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,13 +1,15 @@
+import { APP_GUARD } from '@nestjs/core'
 import { AppController } from './app.controller'
-import { AuthModule } from 'modules/auth/auth.module'
+import { AtGuard } from '../auth/guards/at.guard'
+import { AuthModule } from '../auth/auth.module'
 import { ConfigModule } from '@nestjs/config'
-import { DatabaseModule } from 'database/database.module'
-import { FileUploadModule } from 'modules/files/file-upload.module'
+import { DatabaseModule } from '../../database/database.module'
+import { FileUploadModule } from '../files/file-upload.module'
 import { LoggerMiddleware } from '../../common/middleware/logger.middleware'
 import { MailModule } from '../mail/mail.module'
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common'
 import { MinioClientModule } from '../minio/minio-client.module'
-import { UsersModule } from 'modules/users/users.module'
+import { UsersModule } from '../users/users.module'
 import { validateConfig } from '../config'
 
 @Module({
@@ -27,6 +29,12 @@ import { validateConfig } from '../config'
     UsersModule,
   ],
   controllers: [AppController],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AtGuard,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
