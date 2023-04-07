5803310734e5d131e27e850d1cd680acb633013d
Murf-y <charbelfayad64@gmail.com>
2023-04-07 23:24:55 +0300

    feat: add forgot password template to email

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -1,16 +1,52 @@
+import { ConfigService } from '@nestjs/config'
 import { Injectable } from '@nestjs/common'
 import { MailerService } from '@nestjs-modules/mailer'
+import { User } from 'modules/users/schemas/user.schema'
 
 @Injectable()
 export class MailService {
-  constructor(private mailerService: MailerService) {}
+  private readonly APP_URL
+  constructor(private mailerService: MailerService, private configService: ConfigService) {
+    this.APP_URL = this.configService.get('APP_URL')
+  }
 
   async sendWelcomeEmail(receiverEmail: string) {
-    await this.mailerService.sendMail({
-      to: receiverEmail,
-      subject: 'Welcome to NinjaCo!',
-      template: './welcome', // `.hbs` extension is appended automatically
-      context: {},
-    })
+    try {
+      await this.mailerService.sendMail({
+        to: receiverEmail,
+        subject: 'Welcome to NinjaCo!',
+        template: './welcome', // `.hbs` extension is appended automatically
+        context: {},
+      })
+      return true
+    } catch (error) {
+      console.log(error)
+      return false
+    }
+  }
+
+  async sendForgotPasswordMail(user: User, token: string) {
+    const url = `${this.APP_URL}/auth/reset-password/${token}`
+    try {
+      await this.mailerService.sendMail({
+        to: user.email,
+        subject: 'Reset your password | NinjaCo',
+        template: './forgotpassword', // `.hbs` extension is appended automatically
+        context: {
+          url,
+        },
+        attachments: [
+          {
+            filename: 'logo.svg',
+            path: __dirname + '/templates/logo.svg',
+            cid: 'logo',
+          },
+        ],
+      })
+      return true
+    } catch (error) {
+      console.log(error)
+      return false
+    }
   }
 }
