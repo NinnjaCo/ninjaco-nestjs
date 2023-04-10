da3d56a2fe0b60007f287838176421471382ded6
Murf-y <charbelfayad64@gmail.com>
2023-04-10 18:48:43 +0300

    feat: update dto all optional

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,24 +1,29 @@
-import { IsDateString, IsEmail, IsNotEmpty, IsString } from 'class-validator'
+import { IsDateString, IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator'
 
 export class UpdateUserDto {
+  @IsOptional()
   @IsString()
   @IsNotEmpty()
   firstName: string
 
+  @IsOptional()
   @IsString()
   @IsNotEmpty()
   lastName: string
 
+  @IsOptional()
   @IsString()
   @IsNotEmpty()
   @IsDateString()
   dateOfBirth: string
 
+  @IsOptional()
   @IsString()
   @IsNotEmpty()
   @IsEmail()
   email: string
 
+  @IsOptional()
   @IsString()
   @IsNotEmpty()
   password: string
