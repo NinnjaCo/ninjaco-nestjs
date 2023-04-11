7be23169d685159834fcd6effb49c629469596c9
ahmad <issa.ahmad2002@gmail.com>
2023-04-11 16:35:29 +0300

    fix: fix bugs

--- src/modules/mail/mail.controller.ts
+++ src/modules/mail/mail.controller.ts
@@ -2,17 +2,18 @@ import { Body, Post } from '@nestjs/common'
 import { Controller } from '@nestjs/common'
 import { MailService } from './mail.service'
 import { sendEmailDto } from 'modules/mail/dto/send-email.dto'
+import { EmailEnum } from './mail.enum'
 
 @Controller('send-email')
 export class MailController {
   constructor(private readonly mailService: MailService) {}
   @Post()
-  async sendDeleteUserEmail(@Body() body: sendEmailDto): Promise<boolean> {
-    if (body.emailEnum === 'notify') {
+  async sendEmail(@Body() body: sendEmailDto): Promise<boolean> {
+    if (body.emailEnum === EmailEnum.NOTIFY) {
       return this.mailService.sendNotifyUserEmail(body)
-    } else if (body.emailEnum === 'delete') {
+    } else if (body.emailEnum === EmailEnum.DELETE) {
       return this.mailService.sendDeleteUserEmail(body)
-    } else {
+    } else if (body.emailEnum === EmailEnum.RESET) {
       return this.mailService.sendResetPasswordEmail(body)
     }
   }
