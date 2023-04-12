d35168ca32898bd7ce856b5c33b5270c9a8ed358
tony123bs <toni123bousleiman@gmail.com>
2023-04-12 23:25:56 +0300

    refactor: remove missions an levels repository

--- src/modules/courses/courses.module.ts
+++ src/modules/courses/courses.module.ts
@@ -2,8 +2,9 @@ import { Course, CourseSchema } from './schemas/course.schema'
 import { CoursesController } from './courses.controller'
 import { CoursesRepository } from './courses.repository'
 import { CoursesService } from './courses.service'
-import { LevelsRepository } from './levels.repository'
-import { MissionsRepository } from './missions.repository'
+
+import { MissionSchema } from './schemas/mission.schema'
+
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { RolesModule } from 'modules/roles/roles.module'
@@ -11,7 +12,7 @@ import { RolesModule } from 'modules/roles/roles.module'
 @Module({
   imports: [MongooseModule.forFeature([{ name: Course.name, schema: CourseSchema }]), RolesModule],
   controllers: [CoursesController],
-  providers: [CoursesService, CoursesRepository, MissionsRepository, LevelsRepository],
+  providers: [CoursesService, CoursesRepository],
   exports: [CoursesService],
 })
 export class CoursesModule {}
