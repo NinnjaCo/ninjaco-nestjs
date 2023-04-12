0308318a024343775ceb745739e385f749992349
Raghidkh <raghidkhoury2@gmail.com>
2023-04-12 23:41:45 +0300

    feat: add documentation for fuctions in the courses service

--- src/modules/courses/courses.service.ts
+++ src/modules/courses/courses.service.ts
@@ -14,23 +14,49 @@ export class CoursesService {
     private readonly courseRepository: CoursesRepository,
     private readonly roleService: RolesService
   ) {}
+  /**
+   * Find all courses
+   * @returns Promise <Course[]> if users are found, otherwise empty array
+   */
 
   async findAll(): Promise<Course[]> {
     return await this.courseRepository.find({})
   }
+  /**
+   * Create new course
+   * @param CreateCourseDto
+   * @returns Promise<course>
+   */
 
   async createCourse(courseDto: CreateCourseDto): Promise<Course> {
     const course = await this.courseRepository.create(courseDto)
     return course
   }
 
+  /**
+   * Find user by id
+   * @param courseId
+   * @returns Promise<Course> if course is found, otherwise null
+   */
+
   async findCourseById(courseId: string): Promise<Course> {
     return await this.courseRepository.findOne({ _id: courseId })
   }
+  /**
+   * Update course by id
+   * @param courseId
+   * @param updateCourseDto
+   * @returns Promise<Course> if user is found, otherwise null
+   */
 
   async updateCourse(courseId: string, CreateCourseDto): Promise<Course> {
     return await this.courseRepository.findOneAndUpdate({ _id: courseId }, CreateCourseDto)
   }
+  /**
+   * Delete course by id
+   * @param courseId
+   * @returns Promise<course> if user is found, otherwise null
+   */
 
   async deleteCourse(courseId: string): Promise<Course> {
     return await this.courseRepository.findOneAndDelete({ _id: courseId })
