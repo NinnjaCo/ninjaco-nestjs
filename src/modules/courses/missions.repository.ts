47fec6a39c7bc6911d4a15cd162d0275b4b57c11
tony123bs <toni123bousleiman@gmail.com>
2023-04-12 23:11:09 +0300

    refactor: add repositorie to courses module

--- src/modules/courses/missions.repository.ts
+++ src/modules/courses/missions.repository.ts
@@ -5,7 +5,7 @@ import { Mission, MissionDocument } from './schemas/mission.schema'
 import { Model } from 'mongoose'
 
 @Injectable()
-export class MissionRepository extends EntityRepository<MissionDocument> {
+export class MissionsRepository extends EntityRepository<MissionDocument> {
   constructor(@InjectModel(Mission.name) private readonly MissionModel: Model<MissionDocument>) {
     super(MissionModel)
   }
