fdf60f5b68bb5bc468042bf72bc58f974bb038f4
Raghidkh <raghidkhoury2@gmail.com>
2023-04-12 19:00:28 +0300

    feat: add courses dto

---
+++ src/modules/courses/dto/create-level.dto.ts
@@ -0,0 +1,14 @@
+import { IsArray, IsNotEmpty, IsNumber } from 'class-validator'
+export class CreateLevelDto {
+  @IsNumber()
+  @IsNotEmpty()
+  levelNumber: number
+
+  @IsArray()
+  @IsNotEmpty()
+  buildingParts: string[]
+
+  @IsArray()
+  @IsNotEmpty()
+  stepGuideImage: string[]
+}
---
+++ src/modules/courses/dto/create-mission.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty, IsString } from 'class-validator'
+export class CreateMissionDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string
+
+  @IsString()
+  @IsNotEmpty()
+  description: string
+
+  @IsString()
+  @IsNotEmpty()
+  image: string
+
+  @IsString()
+  @IsNotEmpty()
+  categoryId: string
+}
