cd508f707066bc06b351210d34bc9cf894cf95ef
ahmad <issa.ahmad2002@gmail.com>
2023-04-14 06:27:13 +0300

    fix: fix game api error

--- src/modules/games/games.controller.ts
+++ src/modules/games/games.controller.ts
@@ -3,6 +3,9 @@ import { Body, Controller, Post } from '@nestjs/common'
 import { CreateGamesDto } from './schemas/dto/create-game.dto'
 import { Game } from './schemas/game.schema'
 import { GamesService } from './games.service'
+import { Public } from 'common/decorators/public.decorator'
+import { RoleEnum } from 'modules/roles/roles.enum'
+import { Roles } from 'modules/roles/roles.decorator'
 
 @Controller('game')
 export class GamesController {
@@ -11,6 +14,7 @@ export class GamesController {
     description: 'Create new Game and save to database',
   })
   @Post()
+  @Public()
   create(@Body() userDto: CreateGamesDto): Promise<Game> {
     return this.gamesService.create(userDto)
   }
--- src/modules/games/games.service.ts
+++ src/modules/games/games.service.ts
@@ -1,13 +1,13 @@
 import { CreateGamesDto } from './schemas/dto/create-game.dto'
 import { Game } from './schemas/game.schema'
+import { GamesRepository } from './games.rpeository'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class GamesService {
-  GamesRepository: any
-  constructor() {}
+  constructor(private readonly gamesRepository: GamesRepository) {}
   async create(createDto: CreateGamesDto): Promise<Game> {
-    const createdGame = await this.GamesRepository.create(createDto)
+    const createdGame = await this.gamesRepository.create(createDto)
     return createdGame
   }
 }
