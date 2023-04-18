bd8902e13e4c3f687cbd6a2e1776baea3410b40d
tony123bs <toni123bousleiman@gmail.com>
2023-04-18 15:42:26 +0300

    feat : create update level progress api

--- src/modules/courseManagement/levelProgress.controller.ts
+++ src/modules/courseManagement/levelProgress.controller.ts
@@ -1,12 +1,14 @@
 import { ApiGlobalResponse } from 'common/decorators/api-global-response.decorators'
 import { ApiTags } from '@nestjs/swagger'
 import { ArraySchema } from 'swagger/swagger-primitive-type'
-import { Controller, Delete, Get, Param } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Put } from '@nestjs/common'
 import { Course } from 'modules/courses/schemas/course.schema'
 import { LevelProgress } from './schemas/LevelProgress.schema'
 import { LevelProgressService } from './levelProgress.service'
 import { RoleEnum } from 'modules/roles/roles.enum'
 import { Roles } from 'modules/roles/roles.decorator'
+import { UpdateCourseDto } from 'modules/courses/dto/update-course.dto'
+import { UpdateLevelProgressDto } from './dto/update-levelProgress.dto'
 
 @ApiTags('LeveLProgress')
 @Controller('levelProgress')
@@ -30,12 +32,24 @@ export class LevelProgressController {
     return this.levelProgressService.findLevelProgressById(id)
   }
 
-  @ApiGlobalResponse(Course, {
+  @ApiGlobalResponse(LevelProgress, {
     description: 'Delete LevelProgress by id | ADMIN and creator only',
   })
   @Roles(RoleEnum.ADMIN, RoleEnum.CREATOR)
   @Delete(':id')
   remove(@Param('id') id: string): Promise<LevelProgress> {
     return this.levelProgressService.deleteCourse(id)
+  }
+
+  @ApiGlobalResponse(Course, {
+    description: 'Update LevelProgress | ADMIN and creator only',
+  })
+  @Roles(RoleEnum.ADMIN, RoleEnum.CREATOR)
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() progressDto: UpdateLevelProgressDto
+  ): Promise<LevelProgress> {
+    return this.levelProgressService.updateCourse(id, progressDto)
   }
 }
--- src/modules/courseManagement/levelProgress.service.ts
+++ src/modules/courseManagement/levelProgress.service.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, InternalServerErrorException } from '@nestjs/common'
 import { LevelProgress } from './schemas/LevelProgress.schema'
 import { LevelProgressRepository } from './levelProgress.repository'
+import { MongoServerError } from 'mongodb'
+import { UpdateLevelProgressDto } from './dto/update-levelProgress.dto'
+import { handleMongoDuplicateKeyError } from 'common/shared'
 
 @Injectable()
 export class LevelProgressService {
@@ -15,5 +18,19 @@ export class LevelProgressService {
 
   async deleteCourse(id: string): Promise<LevelProgress> {
     return await this.levelProgressRepository.findOneAndDelete({ _id: id })
+  }
+
+  async updateCourse(id: string, progressDto: UpdateLevelProgressDto): Promise<LevelProgress> {
+    try {
+      return await this.levelProgressRepository.findOneAndUpdate({ _id: id }, progressDto)
+    } catch (error) {
+      // if error type is from mongodb
+      if (error instanceof MongoServerError) {
+        // This will automatically throw a BadRequestException with the duplicate key error message
+        handleMongoDuplicateKeyError(error)
+      } else {
+        throw new InternalServerErrorException(error)
+      }
+    }
   }
 }
