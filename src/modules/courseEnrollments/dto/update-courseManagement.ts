7b6b7db337d9c0cbb65e78da5c281ca8610772ac
Raghidkh <raghidkhoury2@gmail.com>
2023-04-18 20:42:20 +0300

    fix: chnage the update enrollment course dto

--- src/modules/courseEnrollments/dto/update-courseManagement.ts
+++ src/modules/courseEnrollments/dto/update-courseManagement.ts
@@ -6,24 +6,6 @@ import { User } from 'modules/users/schemas/user.schema'
 export class UpdateCourseMangementDto {
   @ApiProperty({ required: false })
   @IsOptional()
-  @IsObject()
-  @IsNotEmpty()
-  user: User
-
-  @ApiProperty({ required: false })
-  @IsOptional()
-  @IsObject()
-  @IsNotEmpty()
-  course: Course
-
-  @ApiProperty({ required: false })
-  @IsOptional()
-  @IsString()
-  @IsNotEmpty()
-  enrolledAt: string
-
-  @ApiProperty({ required: false })
-  @IsOptional()
   @IsBoolean()
   @IsNotEmpty()
   completed: boolean
