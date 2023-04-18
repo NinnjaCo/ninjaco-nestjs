ba5f42651cc0c9840707277d394dee637c11f3f5
Raghidkh <raghidkhoury2@gmail.com>
2023-04-18 17:04:00 +0300

    feat: add courses service

--- src/modules/courseEnrollments/courseEnrollments.service.ts
+++ src/modules/courseEnrollments/courseEnrollments.service.ts
@@ -1,4 +1,18 @@
+import { CourseEnrollment } from './schemas/courseEnrollment.schema'
+import { CourseManagementRepository } from './courseMangement.repository'
+import { CoursesService } from 'modules/courses/courses.service'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class CourseEnrollmentsService {}
+export class CourseEnrollmentsService {
+  constructor(
+    private readonly courseManagementRepository: CourseManagementRepository,
+    private readonly coursesService: CoursesService
+  ) {}
+
+  async findAll(): Promise<CourseEnrollment[]> {
+    //return the all the courses using the findAll function in the course Service
+    const courses = await this.coursesService.findAll()
+    return await this.courseManagementRepository.findCourseMangement(courses)
+  }
+}
