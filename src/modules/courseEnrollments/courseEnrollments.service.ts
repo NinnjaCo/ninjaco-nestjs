676f65dd0d44791e083a22f04c2d2f4ee83d994a
Raghidkh <raghidkhoury2@gmail.com>
2023-04-18 17:11:38 +0300

    fix: change repo import name

--- src/modules/courseEnrollments/courseEnrollments.service.ts
+++ src/modules/courseEnrollments/courseEnrollments.service.ts
@@ -1,18 +1,18 @@
 import { CourseEnrollment } from './schemas/courseEnrollment.schema'
-import { CourseManagementRepository } from './courseMangement.repository'
+import { CourseEnrollmentsRepository } from './courseEnrollments.repository'
 import { CoursesService } from 'modules/courses/courses.service'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class CourseEnrollmentsService {
   constructor(
-    private readonly courseManagementRepository: CourseManagementRepository,
+    private readonly courseEnrollmentRepository: CourseEnrollmentsRepository,
     private readonly coursesService: CoursesService
   ) {}
 
-  async findAll(): Promise<CourseEnrollment[]> {
-    //return the all the courses using the findAll function in the course Service
-    const courses = await this.coursesService.findAll()
-    return await this.courseManagementRepository.findCourseMangement(courses)
-  }
+  //   async findAll(): Promise<CourseEnrollment[]> {
+  //     //return the all the courses using the findAll function in the course Service
+  //     const courses = await this.coursesService.findAll()
+  //     return await this.courseEnrollmentRepository.findCourseMangement(courses)
+  //   }
 }
