9404790d143e43db48d581cc4ed3a6db54e5f1b7
Raghidkh <raghidkhoury2@gmail.com>
2023-04-20 02:05:05 +0300

    fix: change in the create-course dto file

--- src/modules/courseEnrollments/dto/create-courseManagement.dto.ts
+++ src/modules/courseEnrollments/dto/create-courseManagement.dto.ts
@@ -1,19 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsBoolean, IsNotEmpty, IsObject, IsString } from 'class-validator'
+import { IsNotEmpty, IsString } from 'class-validator'
 
 export class CreateCourseManagementDto {
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
   userId: string
-
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
-  enrolledAt: string
-
-  @ApiProperty()
-  @IsBoolean()
-  @IsNotEmpty()
-  completed: boolean
 }
