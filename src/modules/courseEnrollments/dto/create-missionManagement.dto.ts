42844f9cdd82910fd1fb74efdda5fde1485243d5
Raghidkh <raghidkhoury2@gmail.com>
2023-04-19 18:23:15 +0300

    fix: remove unused imports

--- src/modules/courseEnrollments/dto/create-missionManagement.dto.ts
+++ src/modules/courseEnrollments/dto/create-missionManagement.dto.ts
@@ -1,6 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { IsBoolean, IsNotEmpty, IsString } from 'class-validator'
-import { Mission } from 'modules/courses/schemas/mission.schema'
 
 export class CreateMissionManagementDto {
   @ApiProperty()
--- src/modules/courseEnrollments/dto/update-misionManagement.dto.ts
+++ src/modules/courseEnrollments/dto/update-misionManagement.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator'
+import { IsBoolean, IsNotEmpty, IsOptional } from 'class-validator'
 
 export class UpdateMissionManagementDto {
   @ApiProperty({ required: false })
