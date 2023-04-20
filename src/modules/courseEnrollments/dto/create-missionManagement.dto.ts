43dcc882f0919fbc3d4b166903d9cb01b5601029
Raghidkh <raghidkhoury2@gmail.com>
2023-04-20 18:27:07 +0300

    feat: add create mission dto

---
+++ src/modules/courseEnrollments/dto/create-missionManagement.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { IsNotEmpty, IsString } from 'class-validator'
+export class CreateMissionManagementDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  userId: string
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  courseId: string
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  missionId: string
+}
