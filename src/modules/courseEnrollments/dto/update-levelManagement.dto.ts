3e3d200a64721ccad9ec778ae8f8f9f2cf97e02e
Raghidkh <raghidkhoury2@gmail.com>
2023-04-20 21:30:38 +0300

    fix: chane in the update level dto

--- src/modules/courseEnrollments/dto/update-levelManagement.dto.ts
+++ src/modules/courseEnrollments/dto/update-levelManagement.dto.ts
@@ -1,9 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsBoolean, IsNotEmpty, IsOptional } from 'class-validator'
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator'
 export class UpdateLevelManagementDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  userId: string
+
   @ApiProperty({ required: false })
   @IsOptional()
   @IsBoolean()
   @IsNotEmpty()
   completed: boolean
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  courseId: string
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  missionId: string
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  levelId: string
 }
