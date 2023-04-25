d91d7329035633c463d94a413bf391d6ebfa205f
Raghidkh <raghidkhoury2@gmail.com>
2023-04-25 14:56:23 +0300

    feat: add feedbacl repo

---
+++ src/modules/feedbacks/feedbacks.repository.ts
@@ -0,0 +1,12 @@
+import { EntityRepository } from 'database/entity.repository'
+import { Feedback, FeedbackDocument } from './schemas/feedbacks.schema'
+import { InjectModel } from '@nestjs/mongoose'
+import { Injectable } from '@nestjs/common'
+import { Model } from 'mongoose'
+
+@Injectable()
+export class FeedbacksRepository extends EntityRepository<FeedbackDocument> {
+  constructor(@InjectModel(Feedback.name) private readonly feedbackModel: Model<FeedbackDocument>) {
+    super(feedbackModel)
+  }
+}
