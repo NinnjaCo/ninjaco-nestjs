e0f461bd931c9f3553c60ff406499dbe782014f5
Raghidkh <raghidkhoury2@gmail.com>
2023-04-29 17:11:39 +0300

    feat: add course enrollment model testing

---
+++ src/modules/courseEnrollments/test/support/courseEnrollment.model.ts
@@ -0,0 +1,7 @@
+import { CourseEnrollment } from '../../../courseEnrollments/schemas/courseEnrollment.schema'
+import { MockModel } from '../../../../database/test/support/mock.model'
+import { courseEnrollmentStub } from '../stubs/courseEnrollment.stub'
+
+export class CourseEnrollmentModel extends MockModel<CourseEnrollment> {
+  protected entityStub = courseEnrollmentStub()
+}
