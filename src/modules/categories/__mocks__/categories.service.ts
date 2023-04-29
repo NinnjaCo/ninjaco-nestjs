857babe22886c20d98e8345aa23af779ee964e82
ahmad <issa.ahmad2002@gmail.com>
2023-04-29 21:30:49 +0300

    fix: fix previous typo

--- src/modules/categories/__mocks__/categories.service.ts
+++ src/modules/categories/__mocks__/categories.service.ts
@@ -1,4 +1,4 @@
-import { categoryStub } from '../test/support/support/stubs/category.stub'
+import { categoryStub } from '../test/stubs/category.stub'
 
 export const RolesService = jest.fn().mockReturnValue({
   findAll: jest.fn().mockResolvedValue([categoryStub()]),
