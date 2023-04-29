9ab29a1d3012cfcc7d535d33c303d9409813638b
Raghidkh <raghidkhoury2@gmail.com>
2023-04-29 14:58:23 +0300

    fix: change course service testing functions name

--- src/modules/courses/__mocks__/courses.service.ts
+++ src/modules/courses/__mocks__/courses.service.ts
@@ -2,8 +2,8 @@ import { courseStub } from '../test/stubs/course.stub'
 
 export const CoursesService = jest.fn().mockReturnValue({
   findAll: jest.fn().mockResolvedValue([courseStub()]),
-  findOne: jest.fn().mockResolvedValue(courseStub()),
-  create: jest.fn().mockResolvedValue(courseStub()),
-  remove: jest.fn().mockResolvedValue(courseStub()),
-  update: jest.fn().mockResolvedValue(courseStub()),
+  findCourseById: jest.fn().mockResolvedValue(courseStub()),
+  createCourse: jest.fn().mockResolvedValue(courseStub()),
+  deleteCourse: jest.fn().mockResolvedValue(courseStub()),
+  updateCourse: jest.fn().mockResolvedValue(courseStub()),
 })
