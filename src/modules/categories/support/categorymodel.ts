59c7562b83d77efd2fa701c821801ce312b43b02
ahmad <issa.ahmad2002@gmail.com>
2023-04-29 12:33:18 +0300

    fix: delete unuused files

--- src/modules/categories/support/categorymodel.ts
+++
@@ -1,7 +0,0 @@
-import { Category } from '../schemas/category.schema'
-import { MockModel } from '../../../database/test/support/mock.model'
-import { categoryStub } from '../stubs/category.stub'
-
-export class CategoryModel extends MockModel<Category> {
-  protected entityStub = categoryStub()
-}
