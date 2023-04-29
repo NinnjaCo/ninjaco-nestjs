062c53e58d6503890423ef80f44580bdae26619a
Raghidkh <raghidkhoury2@gmail.com>
2023-04-29 18:56:21 +0300

    feat: add mock functions

--- src/modules/courseEnrollments/__mocks__/courseEnrollments.service.ts
+++ src/modules/courseEnrollments/__mocks__/courseEnrollments.service.ts
@@ -5,4 +5,11 @@ export const CourseEnrollmentsService = jest.fn().mockReturnValue({
   findCourseById: jest.fn().mockResolvedValue(courseEnrollmentStub()),
   deleteCourse: jest.fn().mockResolvedValue(courseEnrollmentStub()),
   createCourseEnrollement: jest.fn().mockResolvedValue(courseEnrollmentStub()),
+  createMissionProgress: jest.fn().mockResolvedValue(courseEnrollmentStub()),
+  findAllMissions: jest.fn().mockResolvedValue([courseEnrollmentStub()]),
+  findMissionById: jest.fn().mockResolvedValue(courseEnrollmentStub()),
+  findAllLevels: jest.fn().mockResolvedValue([courseEnrollmentStub()]),
+  findLevelById: jest.fn().mockResolvedValue(courseEnrollmentStub()),
+  createLevelProgress: jest.fn().mockResolvedValue(courseEnrollmentStub()),
+  updateProgress: jest.fn().mockResolvedValue(courseEnrollmentStub()),
 })
