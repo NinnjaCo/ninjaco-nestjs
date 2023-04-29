d0af303819f33ad6fdc3179f760766f08a01e7aa
tony123bs <toni123bousleiman@gmail.com>
2023-04-29 14:28:18 +0300

    fix: add missing imports

--- src/modules/usersLevelsProgress/__mocks__/levelProgress.service.ts
+++ src/modules/usersLevelsProgress/__mocks__/levelProgress.service.ts
@@ -1,3 +1,5 @@
+import { levelProgressStub } from '../test/stubs/levelProgress.stub'
+
 export const LevelProgressService = jest.fn().mockReturnValue({
   findAll: jest.fn().mockResolvedValue([levelProgressStub()]),
   create: jest.fn().mockResolvedValue(levelProgressStub()),
--- src/modules/usersLevelsProgress/test/support/levelProgress.model.ts
+++ src/modules/usersLevelsProgress/test/support/levelProgress.model.ts
@@ -1,5 +1,6 @@
 import { LevelProgress } from 'modules/usersLevelsProgress/schema/LevelProgress.schema'
 import { MockModel } from 'database/test/support/mock.model'
+import { levelProgressStub } from '../stubs/levelProgress.stub'
 
 export class LevelProgressModel extends MockModel<LevelProgress> {
   protected entityStub = levelProgressStub()
