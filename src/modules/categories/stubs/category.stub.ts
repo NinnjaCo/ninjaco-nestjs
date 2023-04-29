00e55d76a0a672748dfe6f541afeaeec30cae646
ahmad <issa.ahmad2002@gmail.com>
2023-04-29 04:57:55 +0300

    fix: fix  categoryStub

--- src/modules/categories/stubs/category.stub.ts
+++ src/modules/categories/stubs/category.stub.ts
@@ -4,6 +4,9 @@ import mongoose from 'mongoose'
 export const categoryStub: () => Category = () => {
   return {
     _id: new mongoose.Types.ObjectId('5f9d5c2e5a6c2a1f9c2aae4f'),
-    categoryName: 'category',
+    categoryName: 'easy',
+    createdAt: 'Thu Apr 20 2023 15:24:53 GMT+0000 (Coordinated Universal Time)',
+    updatedAt: 'Thu Apr 20 2023 15:24:53 GMT+0000 (Coordinated Universal Time)',
+    __v: 0,
   }
 }
