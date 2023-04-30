717d58c07a412a44cd0f47d3a25036b97eef26a3
ahmad <issa.ahmad2002@gmail.com>
2023-04-30 15:02:09 +0300

    fix: remove uneeded dependencies

--- src/modules/usersGameProgress/__mocks__/usersGameProgress.service.ts
+++ src/modules/usersGameProgress/__mocks__/usersGameProgress.service.ts
@@ -1,5 +1,3 @@
-import { GameProgress } from '../schema/game-progress.schema'
-import { userPlayGameStub } from '../test/stubs/userPlayGame.stub'
 import { gameProgressStub } from '../test/stubs/usersGameProgress.stub'
 
 export const gameProgressService = jest.fn().mockReturnValue({
