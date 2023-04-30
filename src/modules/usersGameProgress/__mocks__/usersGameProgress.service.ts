61cd3a930e2ca525d918fc025f00c1fa7686c0d3
ahmad <issa.ahmad2002@gmail.com>
2023-04-30 15:01:36 +0300

    feat: create userGameProgress service test

--- src/modules/usersGameProgress/__mocks__/usersGameProgress.service.ts
+++ src/modules/usersGameProgress/__mocks__/usersGameProgress.service.ts
@@ -1,6 +1,8 @@
+import { GameProgress } from '../schema/game-progress.schema'
 import { userPlayGameStub } from '../test/stubs/userPlayGame.stub'
+import { gameProgressStub } from '../test/stubs/usersGameProgress.stub'
 
-export const UsersGamesProgressService = jest.fn().mockReturnValue({
-  getCompletedGames: jest.fn().mockResolvedValue([userPlayGameStub()]),
-  createUserPlayGameEntry: jest.fn().mockResolvedValue(userPlayGameStub()),
+export const gameProgressService = jest.fn().mockReturnValue({
+  getGameProgress: jest.fn().mockResolvedValue([gameProgressStub()]),
+  create: jest.fn().mockResolvedValue(gameProgressStub()),
 })
